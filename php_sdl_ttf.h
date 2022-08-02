6fa5257bbe2d58890daf6a855206f9f1912cec19
Santiago Lizardo <santiagolizardo@users.noreply.github.com>
2022-08-02 22:29:26 +0100

    Update package file

--- php_sdl_ttf.h
+++ php_sdl_ttf.h
@@ -6,7 +6,7 @@
 extern zend_module_entry sdl_ttf_module_entry;
 #define phpext_sdl_ttf_ptr &sdl_ttf_module_entry
 
-#define PHP_SDL_TTF_VERSION "0.2.0"
+#define PHP_SDL_TTF_VERSION "0.3.0"
 
 #if defined(ZTS) && defined(COMPILE_DL_SDL_TTF)
 ZEND_TSRMLS_CACHE_EXTERN()
